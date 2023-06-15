7a15c8e6ee8c454dc987ca69a313713c41e49134
62822 <delvinputra805@gmail.com>
2023-06-15 16:12:40 +0700

    Add project files.

---
+++ stackusingarrayy.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Program {
+private:
+	string stack_array[5];
+	int top;
+
+public:
+	Program() {
+		top = -1;
+	}
+
+	string push(string element) {
+		if (top == 4) {
+			cout << "stack full" << endl;
+			return "";
+		}
+
+		top++;
+		stack_array[top] = element;
+
+		cout << endl;
+		cout << element << "ditambahkan(pushed)." << endl;
+
+		return element;
+	}
+
+	void pop() {
+		if (empty()) {
+			cout << "\nstack is empty. cannot pop." << endl;
+			return;
+
+		}
+
+		cout << "\nThe popped element is: " << stack_array[top] << endl;
+		top--;
+	}
+
+	bool empty() {
+		return (top == -1);
+	}
+
+	void display() {
+		if (empty()) {
+			cout << "\nStack is empty." << endl;
+		}
+		else {
+			for (int tmp = top; tmp >= 0; tmp--) {
+				cout << stack_array[tmp] << endl;
+			}
+		}
+	}
+};
+int main() {
+	Program s;
+
+	while (true) {
+		cout << endl;
+		cout << "\n**Stack Menu**\n";
+		cout << "1. Push\n";
+		cout << "2. Pop\n";
+		cout << "3. Display\n";
+		cout << "4. Exit\n";
+		cout << "\nEnter your choice: ";
+		string input;
+		getline(cin, input);
+		char ch = (input.empty() ? '0' : input[0]);
+		switch (ch) {
+		case '1': {
+			cout << "\nEnter an element: ";
+			string element;
+			getline(cin, element);
+			s.push(element);
+			break;
+		}
+		case '2':
+			if (s.empty()) {
+				cout << "\nStack is empety." << endl;
+				break;
+			}
+			s.pop();
+			break;
+		case'3':
+			s.display();
+			break;
+		case'4':
+			return 0;
+		default:
+			cout << "\nInvalid choice." << endl;
+			break;
+		}
+	}
+}
